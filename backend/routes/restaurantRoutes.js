d0efae5bb26f0a653c41f43a08368a8788b19217
Anaru03 <dele22428@uvg.edu.gt>
2025-04-28 16:21:31 -0600

    Estrucutra fornted

--- backend/routes/restaurantRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const restaurantController = require('../controllers/restaurantController');
-
-// Rutas para los restaurantes
-router.post('/', restaurantController.createRestaurant);
-router.get('/', restaurantController.getRestaurants);
-
-module.exports = router;
